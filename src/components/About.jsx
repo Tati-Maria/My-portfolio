a88e849ab46f2c6502a8795c3764cb0893f9b1f4
Maria Tati <mauramaria92@gmail.com>
2023-01-26 11:43:50 +0000

    added meta tags

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             </p>
         </div>
         <div className="flex items-center justify-center">
-          <img src={me} alt="Maria's photo" className="rounded-md w-2/3 lg:w-full" />
+          <img src={me} alt="Maria's photo front-end developer" className="rounded-md w-2/3 lg:w-full" />
         </div>
     </section>
     </Layout>
